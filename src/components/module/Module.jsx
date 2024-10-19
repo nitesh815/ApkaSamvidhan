cbdc9bdfdae590c7619beef9db686bee0ca5ce1b
Meenakshi Srivastava <imeenakshi14245@gmail.com>
2024-10-19 19:20:54 +0530

    added quiz

--- src/components/module/Module.jsx
+++ src/components/module/Module.jsx
@@ -2,24 +2,26 @@ import React from "react"
 import Back from "../common/back/Back"
 import TeamCard from "../team/TeamCard"
 import Awrapper from "../about/Awrapper"
-import "../about/about.css"
+
 import Sidebar from "./Sidebar"
 import Quiz from "./quiz"
+import "./quiz.css"
 
 const Module = () => {
   return (
     <>
-      <Back title='Team' />
+      <Back title='M1' />
       
         <div class="flex">
       <Sidebar/>
         <div className='container'>
         <>
+        <div className="container1">
         <Quiz/>
+        </div>
         </>
         </div>
         </div>
-      <Awrapper />
     </>
   )
 }
